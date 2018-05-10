5a6cc93a16d1166bd5a51f1833a79a9b13385e15
manjunath-m-manja <manjunath.m.manja@outlook.com>
2018-05-10 16:32:25 +0530

    First Commit

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import predict_7_stages as p7s
+import predict_benign_normal_malignant as bnm
+import predict_dense_fatty_glandular as dfg
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html', path = None)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    imgNo = request.form['name']
+    print imgNo
+    pType = os.popen('python predict_benign_normal_malignant.py '+imgNo).read().strip()
+    pType = pType.split(',')
+    pBack = os.popen('python predict_dense_fatty_glandular.py '+imgNo).read().strip()
+    pBack = pBack.split(',')
+    pAbnorm = os.popen('python predict_7_stages.py '+imgNo).read().strip()
+    pAbnorm = pAbnorm.split(',')
+    return render_template('index.html', path = imgNo,
+                            pType = pType[0], pType1 = pType[1],
+                            pBack = pBack[0], pBack1 = pBack[1],
+                            pAbnorm = pAbnorm[0],pAbnorm1 = pAbnorm[1])
+
+if __name__ == "__main__":
+    p7s.load()
+    bnm.load()
+    dfg.load()
+    app.run(host='0.0.0.0', port=8080, debug=True)
