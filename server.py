780e1fb8fb389e9c3afe478b8d9cbc104948324f
manjunath-m-manja <manjunath.m.manja@outlook.com>
2018-05-10 16:44:00 +0530

    Prob added

--- server.py
+++ server.py
@@ -17,10 +17,16 @@ def process():
     print imgNo
     pType = os.popen('python predict_benign_normal_malignant.py '+imgNo).read().strip()
     pType = pType.split(',')
+    prob = float(pType[1])
+    pType[1] = "{:.3}%".format(prob*100)
     pBack = os.popen('python predict_dense_fatty_glandular.py '+imgNo).read().strip()
     pBack = pBack.split(',')
+    prob = float(pBack[1])
+    pBack[1] = "{:.3}%".format(prob*100)
     pAbnorm = os.popen('python predict_7_stages.py '+imgNo).read().strip()
     pAbnorm = pAbnorm.split(',')
+    prob = float(pAbnorm[1])
+    pAbnorm[1] = "{:.3}%".format(prob*100)
     return render_template('index.html', path = imgNo,
                             pType = pType[0], pType1 = pType[1],
                             pBack = pBack[0], pBack1 = pBack[1],
