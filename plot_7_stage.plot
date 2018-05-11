a92725255391b45b084366c3c1103ef2f5dd9fe0
rajeev ranjan <rajeev.gamerstylo@gmail.com>
2018-05-11 11:52:00 +0530

    plot files added

--- plot_7_stage.plot
+++ plot_7_stage.plot
@@ -3,4 +3,7 @@ from matplotlib import pyplot as plt
 
 arr = np.load('plot_fat_dense_glandular.npy')
 plt.plot(arr)
+plt.title("Accuracy graph for Background tissue")
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
 plt.show()
--- plot_benign_normal_malignant.py
+++ plot_benign_normal_malignant.py
@@ -1,6 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-arr = np.load('plot_benign_normal_malignant.npy')
+arr = np.load('plot_fat_dense_glandular.npy')
 plt.plot(arr)
+plt.title("Accuracy graph for tumour type")
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
 plt.show()
